7bd638d24b82111add773fc1b1899d36b548028e
abhi <abhijeetvc7@gmail.com>
2021-11-11 21:44:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+//console.log(module);
+// require
+
+//import http module
+var http=require('http')
+
+// var server=http.createServer(function(req,res){
+//     if(req.url=='/'){
+//         res.end('Hellooooo')
+//     }
+//     else if(req.url=='/check'){
+//         res.end('Hiiii')
+//     }
+    
+// })
+
+var server=http.createServer((req,res)=>{
+    if(req.url=='/saveData'){
+    if(req.method=='POST'){
+        var jsonString=''
+       // console.log(req);
+        req.on('data',function(data){
+            jsonString+=data
+           // console.log(jsonString);
+        })
+        req.on('end',function(){
+            console.log(JSON.parse(jsonString));    
+        })
+    }
+}})
+
+server.listen(8989)
+
+console.log('Server Started!!!');
+
+// http://localhost:8989/saveData
+
+
+

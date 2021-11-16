586f28ef7f22796b14153a402344f0006e0c5c42
abhi <abhijeetvc7@gmail.com>
2021-11-16 21:45:46 +0530

    ExpressJS - POST request

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express=require('express')
 
+//const bodyParser=require('body-parser')
+
+const cors=require('cors')
+
 const app=express()
 
 const port=8787
 
+var corsOptions={
+    origin:"http://localhost:3000"
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+//app.use(bodyParser.json())
+
 app.get('/check',(req,res)=>{
     res.send('hello ExpressJS')
+})
+
+app.post("/saveData",(req,res)=>{
+    const user=req.body 
+    console.log(user);
+    
 })
 
 app.listen(port,()=>{
